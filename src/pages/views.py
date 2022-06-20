b9c46ea78202ddc5b40cddf79acd9aff57ccef64
Azul <svcredblu@gmail.com>
2022-06-19 19:42:28 -0500

    added methods for view pages

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,4 +3,29 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request, *args, **kwargs):
-    return render(request, "home.html", {})
+    context = {
+        'title' : 'Home'
+    }
+    return render(request, "home.html", context)
+
+
+def blog(request, *args, **kwargs):
+    context = {
+        'title' : 'Blog'
+    }
+    return render(request, "blog.html", context)
+
+
+def contact(request, *args, **kwargs):
+    context = {
+        'title' : 'Contact'
+    }
+    return render(request, "contact.html", context)
+
+
+def passwordGenerator(request, *args, **kwargs):
+    context = {
+        'title': 'Password Generator',
+    }
+
+    return render(request, 'passwordGenerator.html', context)
