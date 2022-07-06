09f3e6d4616a74cffc7af60d770ec932d8afd1c3
Azul <svcredblu@gmail.com>
2022-07-06 12:30:18 -0500

    imported view methods from generic lib

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
 
-class PostList(generic.ListView):
+class PostList(ListView):
     queryset= Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog.html'
 
-class PostDetail(generic.DetailView):
+class PostDetail(DetailView):
     model = Post
     template_name= 'post_detail.html'
