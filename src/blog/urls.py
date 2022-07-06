812b21bcec7964dfe704b603a95f8c88c0fae511
Azul <svcredblu@gmail.com>
2022-07-06 14:32:12 -0500

    added url route to addPosts

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('addPost/', views.AddPost.as_view(), name='addPost'),
     path('<slug:slug>/', views.PostDetail.as_view(),  name='post_detail'),
 ]
